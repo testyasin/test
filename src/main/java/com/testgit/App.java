675b5d891d9c68acbdda95e316f40c6d676439ea
Yasin Memic <yasin.memic@hotmail.com>
2020-03-14 17:40:37 +0300

    The project was started

---
+++ src/main/java/com/testgit/App.java
@@ -0,0 +1,14 @@
+package com.testgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created By Yasin Memic on Mar, 2020
+ */
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class,args);
+    }
+}
